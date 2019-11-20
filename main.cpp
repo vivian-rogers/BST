4e8e1378da85d258208fe0c0bb0601bd2b049eb0
will-rogers <rogers@LAPTOP-5OQ4DUTC.localdomain>
2019-11-19 20:11:06 -0600

    done

---
+++ main.cpp
@@ -0,0 +1,71 @@
+// Sample driver
+// EE 312 Fall 2018
+//
+// The BST should work for any data type or object.
+// Other user created objects must have the << operator overloaded for 
+// output.
+//
+
+#include <iostream>
+#include <fstream>
+#include "BST312.h"
+
+using namespace std;
+
+template <typename T>
+void print(vector<T> vec){
+    for(int i = 0 ; i < vec.size() ; i++){
+        cout << vec[i] << endl;;
+    }
+}
+
+int main() {
+
+    ifstream inFile;
+    inFile.open("test.txt");
+
+    BST_312<string> bst;
+   
+    string s;
+    inFile >> s;
+    while (inFile) {;
+        cout << "inserting ... " << s << endl;
+        bst.insertItem(s);
+        inFile >> s;
+    }
+
+    cout << "postorder traversal is " << endl;
+    print(bst.postOrderTraversal());
+    cout << endl;
+
+    cout << "preorder traversal is " << endl;
+    print(bst.preOrderTraversal());
+    cout << endl;
+
+    cout << "inorder traversal is " << endl;
+    print(bst.inOrderTraversal());
+    cout << endl << endl;
+
+
+    cout << "Remove items " << endl;
+    cout << "number of nodes in tree before delete is " << bst.countNodes() << endl;
+    s = "All";
+    bst.deleteItem(s);
+    s = "I";
+    bst.deleteItem(s);
+    s = "is";
+    bst.deleteItem(s);
+    s = "a";
+    bst.deleteItem(s);
+    s = "voice,";
+    bst.deleteItem(s);
+    
+    cout << "hiii number of nodes in tree after delete is " << bst.countNodes() << endl;
+    cout << endl;
+
+    cout << "inorder traversal is " << endl;
+    print(bst.inOrderTraversal());
+    cout << "done" <<endl;
+    inFile.close();
+}
+
